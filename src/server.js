2dd5a6398423e2d95b855151228e7972c2e358e3
Ayush Shukla <shuklaayush628@gmail.com>
2025-07-12 13:13:37 +0530

    style: format code for consistency and readability

--- src/server.js
+++ src/server.js
@@ -1,24 +1,31 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import { config } from './config.js';
-import { connectDB } from './db.js';
-import resumeRoutes from './api/resume.routes.js'
+import express from "express";
+import cors from "cors";
+import { config } from "./config.js";
+import { connectDB } from "./db.js";
+import resumeRoutes from "./api/resume.routes.js";
 
 const app = express();
 connectDB();
 
-app.use(cors({ origin: config.cors.clientUrl }));
-app.use(express.json({ limit: '50mb' }));
+app.use(
+  cors({
+    origin: config.cors.clientUrl,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 204
+  })
+);
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/resume', resumeRoutes);
+app.use("/api/resume", resumeRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Smart Resume Builder API is running!');
-})
+app.get("/", (req, res) => {
+  res.send("Smart Resume Builder API is running!");
+});
 
 app.listen(config.server.port, () => {
-    console.log(`Server is running on port ${config.server.port}`);
+  console.log(`Server is running on port ${config.server.port}`);
 });
